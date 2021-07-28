8ce42b0343f5bbcfa99fa86106173ce639b2401b
samantha-tran <stran322001@gmail.com>
2021-07-28 17:41:03 +1000

    scraper can find image url of elements in current view of driver

--- scraper.py
+++ scraper.py
@@ -4,14 +4,13 @@ import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from PIL import Image
 
 try:
     from test_details import EMAIL, PASSWORD
 except Exception as e:
     print(e)
-
-MAX_SCROLLS = 20
 
 # get image urls from pinterest board link
 # for image url of each image
@@ -46,22 +45,21 @@ class Scraper:
         self.driver.get(self.boardURL)
         #wait for page to load - for larger boards, increase sleep time
         sleep(10)
-        for i in range(0, MAX_SCROLLS):
-            imgs = self.driver.find_elements_by_tag_name("img")
-            for img in imgs:
-                imageURL = img.get_attribute("src")
-                if (imageURL in self.imageURLs):
-                    break
-                else:
-                    self.imageURLs.append(img.get_attribute("src"))
-            self.driver.execute_script("window.scrollTo(0,{0})".format(i * (self.driver.get_window_size()["height"])))
+        imgs = self.driver.find_elements_by_xpath("//img[@alt!='']")
+        print(len(imgs))
+        for img in imgs:
+            imageURL = img.get_attribute("src")
+            sleep(2)
+            self.imageURLs.append(img.get_attribute("src"))
+        print(self.imageURLs)
+        print(len(self.imageURLs))
 
-    def _find_element(self):
-        element = self.driver.find_elements_by_id("data")
-        if element:
-            return element
-        else:
-            return False
+
+    def _get_scroll_amount(self):
+        scrollHeight =  self.driver.execute_script("return document.body.scrollHeight")
+        windowSize = self.driver.get_window_size()["height"]
+        return scrollHeight // windowSize
+
 
 def main():
     if (len(sys.argv) != 2):
